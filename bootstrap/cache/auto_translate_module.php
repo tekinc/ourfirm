cabefe14f77f4f40002b590f8a5620645ec3de6c
Tarek Ahmed <tarek.ahmed@vertexone.net>
2025-03-08 01:34:16 -0500

    add

---
+++ bootstrap/cache/auto_translate_module.php
@@ -0,0 +1,13 @@
+<?php return array (
+  'providers' => 
+  array (
+    0 => 'Modules\\AutoTranslate\\Providers\\AutoTranslateServiceProvider',
+  ),
+  'eager' => 
+  array (
+    0 => 'Modules\\AutoTranslate\\Providers\\AutoTranslateServiceProvider',
+  ),
+  'deferred' => 
+  array (
+  ),
+);
---
+++ bootstrap/cache/performance_module.php
@@ -0,0 +1,15 @@
+<?php return array (
+  'providers' => 
+  array (
+    0 => 'Modules\\Performance\\Providers\\PerformanceServiceProvider',
+    1 => 'Modules\\Performance\\Providers\\EventServiceProvider',
+  ),
+  'eager' => 
+  array (
+    0 => 'Modules\\Performance\\Providers\\PerformanceServiceProvider',
+    1 => 'Modules\\Performance\\Providers\\EventServiceProvider',
+  ),
+  'deferred' => 
+  array (
+  ),
+);
